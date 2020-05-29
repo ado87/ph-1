bfe227686187963525deda767f1fc0f150243233
ado87 <63777512+ado87@users.noreply.github.com>
2020-05-29 17:12:31 +0900

    Add files via upload

--- js/music.js
+++ js/music.js
@@ -5,12 +5,22 @@ $(function(){
 
         if(cat_name !== '#album_list'){
             // 일반 카테고리
-            $('#album_list ul').hide(200);
-            $(cat_name).show();
+            $('#album_list ul').fadeOut(200);
+            $(cat_name).fadeIn();
         } else {
             // all 버튼을 누른 경우
-            $('#album_list ul').show(200);
+            $('#album_list ul').fadeIn(200);
         }
         return false;
    });
+   $('#album > nav > a').click(function(){
+    // 클래스 제거(상태 초기화)
+    $('#album > nav > a').removeClass('selected');
+
+    // 클래스 추가
+    $(this).addClass('selected');
+
+    // a태그 하이퍼링크 기본 이벤트 해제
+    return false;
+    });
 });
